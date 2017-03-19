44a9f52038a607386e3b5ee08c6d70f5cd9ad52e
Ahmad Milzam <ahmadmilzam91@gmail.com>
2017-03-19 23:51:12 +0700

    Initial commit, components intro

---
+++ src/components/Status.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="c-status">
+    <status-item v-for="item in 3"></status-item>
+  </div>
+</template>
+
+<script>
+  import statusItem from './StatusItem.vue';
+
+  export default {
+    components: {
+      'status-item': statusItem,
+    },
+  };
+</script>
+
+<style lang="scss">
+</style>
