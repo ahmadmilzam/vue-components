86148db3cdd76fa19e420046ee5eeb97aa9a83e4
Ahmad Milzam <ahmadmilzam91@gmail.com>
2017-03-24 20:48:09 +0700

    Dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
-  <div id="app-container" class="o-wrapper">
-    <quote></quote>
+  <div id="app-container" class="o-wrapper u-padding-top u-padding-bottom">
+    <button type="button" @click="selectedComponent = 'quote'">Quote</button>
+    <button type="button" @click="selectedComponent = 'newQuote'">New</button>
+    <button type="button" @click="selectedComponent = 'author'">Author</button>
+    <component :is="selectedComponent">
+      <blockquote cite="http://example.com/facts">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+      </blockquote>
+    </component>
+    <!-- <quote>
+      <blockquote cite="http://example.com/facts">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.</p>
+      </blockquote>
+    </quote> -->
+    <p>
+      Selected Component = {{ selectedComponent }}
+    </p>
   </div>
 </template>
 
 <script>
   import Quote from './components/quotes/Quote.vue';
+  import New from './components/quotes/New.vue';
+  import Author from './components/quotes/Author.vue';
 
   export default {
+    data() {
+      return {
+        quoteTitle: 'The Quote',
+        selectedComponent: 'quote',
+      };
+    },
     components: {
       quote: Quote,
+      author: Author,
+      newQuote: New,
     },
   };
 </script>
--- src/components/quotes/Author.vue
+++ src/components/quotes/Author.vue
@@ -14,7 +14,7 @@
   export default {
     data() {
       return {
-        title: 'Your Awesome Quote',
+        title: 'Your Author',
       };
     },
   };
