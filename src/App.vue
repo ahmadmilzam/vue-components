f728b4fbeba7d37542a5f35cd3e8e5be0373cf20
Ahmad Milzam <ahmadmilzam91@gmail.com>
2017-03-28 21:22:53 +0700

    Custom directive

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
-  <div id="app-container" class="o-wrapper u-padding-top u-padding-bottom">
-    <form-basic></form-basic>
+  <div id="app-container" class="o-wrapper u-padding-top u-padding-bottom" style="background-color: #f5f5f5;">
+    <div class="o-layout">
+      <div class="o-layout__item">
+        <ul>
+          <li>Item 1 (No directive)</li>
+          <li v-hl-basic>Item 2</li>
+          <li v-hl-value="'#edf8e8'">Item 3</li>
+          <li v-hl-arg:background="'#e6b138'">Item 4 (highlight-arg with arg)</li>
+          <li v-hl-arg="'#e6b138'">Item 5 (highlight-arg without arg)</li>
+          <li v-hl-mod.delayed="'#4e9bf6'">Item 6 (Delayed for 3s)</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import FormBasic from './components/forms/FormBasic.vue';
-
-  export default {
-    data() {
-      return {
-        quoteTitle: 'The Quote',
-        selectedComponent: 'quote',
-      };
-    },
-    components: {
-      formBasic: FormBasic,
-    },
-  };
+  export default {};
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,44 @@ import App from './App.vue';
 
 import './scss/main.scss';
 
+Vue.directive('hl-basic', {
+  bind(el) {
+    const element = el;
+    element.style.backgroundColor = '#ffe6e9';
+  },
+});
+
+Vue.directive('hl-value', {
+  bind(el, binding) {
+    const element = el;
+    element.style.backgroundColor = binding.value;
+  },
+});
+
+Vue.directive('hl-arg', {
+  bind(el, binding) {
+    const element = el;
+    if (binding.arg === 'background') {
+      element.style.backgroundColor = binding.value;
+    } else {
+      element.style.color = binding.value;
+    }
+  },
+});
+
+Vue.directive('hl-mod', {
+  bind(el, binding) {
+    const element = el;
+    let timeout = 0;
+    if (binding.modifiers.delayed) {
+      timeout = 3000;
+    }
+    setTimeout(() => {
+      element.style.backgroundColor = binding.value;
+    }, timeout);
+  },
+});
+
 window.app = new Vue({
   el: '#app',
   render: createElement => createElement(App),
