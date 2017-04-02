4b468512793938bc6542ee4eb7948674f7f70869
Ahmad Milzam <ahmadmilzam91@gmail.com>
2017-04-02 19:20:15 +0700

    Filter and mixins final

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,36 @@
   <div id="app-container" class="o-wrapper u-padding-top u-padding-bottom" style="background-color: #f5f5f5;">
     <div class="o-layout">
       <div class="o-layout__item">
-        <ul>
-          <li>Item 1 (No directive)</li>
-          <li v-hl-basic>Item 2</li>
-          <li v-hl-value="'#edf8e8'">Item 3</li>
-          <li v-hl-arg:background="'#e6b138'">Item 4 (highlight-arg with arg)</li>
-          <li v-hl-arg="'#e6b138'">Item 5 (highlight-arg without arg)</li>
-          <li v-hl-mod.delayed="'#4e9bf6'">Item 6 (Delayed for 3s)</li>
+        <h2>{{ title }}</h2>
+        <ul v-if="fruits.length">
+          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
         </ul>
+        <input type="text" class="c-input" name="fruit-filter" v-model="filterText">
       </div>
     </div>
+    <hr>
+    <filter-list></filter-list>
   </div>
 </template>
 
 <script>
-  export default {};
+  import { FruitsList, Title1 } from './mixins/FruitListMixin';
+  import FilterList from './components/filterlist/List.vue';
+
+  export default {
+    mixins: [FruitsList],
+    data() {
+      return {
+        title: Title1,
+      };
+    },
+    filters: {
+      toUppercaseWord(str) {
+        return str.toUpperCase();
+      },
+    },
+    components: {
+      filterList: FilterList,
+    },
+  };
 </script>
---
+++ src/components/filterlist/List.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="o-layout">
+    <div class="o-layout__item">
+      <h2>{{ title }}</h2>
+      <ul v-if="fruits.length">
+        <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+      </ul>
+      <input type="text" class="c-input" name="fruit-filter" v-model="filterText">
+    </div>
+  </div>
+</template>
+
+<script>
+  import { FruitsList, Title2 } from '../../mixins/FruitListMixin';
+
+  export default {
+    mixins: [FruitsList],
+    data() {
+      return {
+        title: Title2,
+      };
+    },
+    filters: {
+      toUppercaseWord(str) {
+        return str.toUpperCase();
+      },
+    },
+  };
+</script>
