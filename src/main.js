7ee99b2209f7d1f5d99d926c401e3e4d3d89b3bb
Ahmad Milzam <ahmadmilzam91@gmail.com>
2017-04-16 21:16:59 +0700

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,33 +1,31 @@
 import Vue from 'vue';
-// import VueRouter from 'vue-router';
+import VueRouter from 'vue-router';
 
 import CentralStore from './store/index';
 import App from './App.vue';
-// import routes from './routes';
+import routes from './routes';
 import './scss/main.scss';
 
-// Vue.use(VueRouter);
+Vue.use(VueRouter);
 
-// console.log(routes);
-
-// const router = new VueRouter({
-  // routes,
-//   mode: 'history',
-//   scrollBehavior(to, from, savedPosition) {
-//     if (savedPosition) {
-//       return savedPosition;
-//     }
-//     if (to.hash) {
-//       return {
-//         selector: to.hash,
-//       };
-//     }
-//     return { x: 0, y: 0 };
-//   },
-// });
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return {
+        selector: to.hash,
+      };
+    }
+    return { x: 0, y: 0 };
+  },
+});
 
 window.app = new Vue({
-  // router,
+  router,
   store: CentralStore,
   el: '#app',
   render: createElement => createElement(App),
